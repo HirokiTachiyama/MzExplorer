ddd3c03139067556b1d5a61b19383f270a2bcfa2
tachiyama <hirokitachiyama@gmail.com>
2025-11-19 17:40:28 +0900

    init

---
+++ MzExplorer/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using MzExplorer.Properties;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace MzExplorer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "N/A";
+            BuildInfoTextBlock.Text = $"MzExplorer Version: v{version}.{BuildInfo.BuildCount} / Build: {BuildInfo.BuildDateTime}";
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                SelectedFileNameTextBlock.Text = System.IO.Path.GetFileName(dialog.FileName);
+            }
+        }
+
+    }
+}
---
+++ MzExplorer/Properties/BuildInfo.g.cs
@@ -0,0 +1,3 @@
+namespace MzExplorer.Properties { internal static class BuildInfo { public const int BuildCount = 
+			  3
+; public const string BuildDateTime = "20251119.17:39:19"; } }
